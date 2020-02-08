94a975707734efbbb5ff42e6a5c5126d04758ddb
apostolis1 <58891482+apostolis1@users.noreply.github.com>
2020-02-09 00:44:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,6 @@ from time import sleep
 
 
 #TODO Implement wait instead of sleep()
-#TODO Devide into methods for more readeable codee
-#TODO check how we found click in other file and implemet that on the toLike method
 
 class InstaBot:
     
@@ -75,18 +73,20 @@ class InstaBot:
                             print("Liked one photo from", username)
         print("Proccess terminated succesfully. Liked" , likedPosts, "posts")
     
-    def __init__(self, us, pw, posts):#takes the username and password of the user as parameters
+    def __init__(self, us, pw, posts, extUsers, tocheck):#takes the username and password of the user as parameters
         self.NO_POSTS_TO_LIKE = posts #the number of top posts of the feed that we will look through
-        self.CheckTheList = True #determines if we look at specific usernames or all the posts
+        self.CheckTheList = tocheck #determines if we look at specific usernames or all the posts
         self.password = pw
         self.username = us
         self.driver = webdriver.Chrome()
         self.usersToLike = []
+        for user in extUsers:
+            self.addUser(user)
         for i in ["euroleague", "nba", "bleacherreport", "overtime"]:
             self.addUser(i)
             
 if __name__ == "__main__":    
-    mybot = InstaBot("", "", 1)
+    mybot = InstaBot("", "", 1, [], True)
     mybot.signIn()
     mybot.like()
     print("test")
