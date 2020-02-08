9194a07ea9bc1b304a01026a147d8f533e3116f5
apostolis1 <58891482+apostolis1@users.noreply.github.com>
2020-02-08 22:46:05 +0200

    Added save file for users

--- gui.py
+++ gui.py
@@ -4,13 +4,34 @@ import bot
 import os
 import string
 
+#TODO add delete button for item in list
+
+myUSERS = []
+
+def loadList():#called in the initlization to load the users from the save.txt file
+    if os.path.isfile("save.txt"):
+        with open("save.txt", "r") as f:
+            tempUsers = f.read()
+        global myUSERS
+        myUSERS = tempUsers.split(',')
+        myUSERS[:] = [user for user in myUSERS if user]#remove empty space
+        print(myUSERS)
+
+
+def updateList():
+    print(myUSERS)
+    with open("save.txt", "w") as f:
+        for user in myUSERS:
+            f.write(user + ',')
+
 def addUser():
     temp = accountEntry.get()
     temp = temp.strip()
     if temp != "":
-        accountsList.insert(tk.END, temp)   
-    accountEntry.delete(first=0, last=tk.END)
-
+        accountsList.insert(tk.END, temp)
+        myUSERS.append(temp)
+    updateList()
+    accountEntry.delete(0, tk.END)
 
 def Login():
     password = pwEntry.get()
@@ -22,37 +43,46 @@ def Login():
     mybot = bot.InstaBot(username, password, postNumber)
     print(username, password)
 
+def initilizeList():#Initilize with past elements from save.txt
+    for user in myUSERS:
+        accountsList.insert(tk.END, user)
+
+
 root = tk.Tk()
 
 #Creating and placing the login grid
 LoginInterface = tk.Frame(root)
 userEntry = tk.Entry(LoginInterface)
 pwEntry = tk.Entry(LoginInterface)
-nmbrOfPostsEntry= tk.Entry(LoginInterface)
-tk.Label(LoginInterface, text = "Username").grid(row = 0)
-tk.Label(LoginInterface, text = "Password").grid(row = 1)
-tk.Label(LoginInterface, text = "Number of posts to like").grid(row = 2)
-userEntry.grid(row = 0, column = 1)
-pwEntry.grid(row = 1, column = 1)
-nmbrOfPostsEntry.grid(row = 2, column = 1)
-loginBtn = tk.Button(LoginInterface, text = "Go", command = Login)
-loginBtn.grid(row = 3, column = 1)
+nmbrOfPostsEntry = tk.Entry(LoginInterface)
+tk.Label(LoginInterface, text="Username").grid(row=0)
+tk.Label(LoginInterface, text="Password").grid(row=1)
+tk.Label(LoginInterface, text="Number of posts to like").grid(row=2)
+userEntry.grid(row=0, column=1)
+pwEntry.grid(row=1, column=1)
+nmbrOfPostsEntry.grid(row=2, column=1)
+loginBtn = tk.Button(LoginInterface, text="Go", command=Login)
+loginBtn.grid(row=3, column=1)
 
 #Accounts to like interface
 AccountsToLikeInterface = tk.Frame(root)
 accountsList = tk.Listbox(AccountsToLikeInterface)
-accountsList.grid(row = 0, column = 0)
+accountsList.grid(row=0, column=0)
 accountEntry = tk.Entry(AccountsToLikeInterface)
-accountEntry.grid(row = 1, column = 0)
-addBtn = tk.Button(AccountsToLikeInterface, text = "Add", command = addUser)
-addBtn.grid(row = 1, column = 1)
+accountEntry.grid(row=1, column=0)
+addBtn = tk.Button(AccountsToLikeInterface, text="Add", command=addUser)
+addBtn.grid(row=1, column=1)
 root.update_idletasks()
 
-#TODO add a file to save the list of users
 
-LoginInterface.place(height = 100, width = 400, x = 0, y = 0)
+LoginInterface.place(height=100, width=400, x=0, y=0)
 root.update_idletasks()
-AccountsToLikeInterface.place(x = 0, y = LoginInterface.winfo_height())
-root.minsize(400,400)
+AccountsToLikeInterface.place(x=0, y=LoginInterface.winfo_height())
+root.minsize(400, 400)
+
+
+
+loadList()
+initilizeList()
+
 root.mainloop()
-
